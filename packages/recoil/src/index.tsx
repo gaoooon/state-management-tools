1c4b3ce90709872451555902b324369fc0dc0056
gaoooon <gaon9749@gmail.com>
2023-10-19 17:16:39 +0900

    Update index.tsx

--- packages/recoil/src/index.tsx
+++ packages/recoil/src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import GlobalStyle from "./globalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <RecoilRoot>
+    <GlobalStyle />
     <App />
   </RecoilRoot>
 );
