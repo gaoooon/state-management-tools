6af5e50eed68dd5d44e6088aa4439db8a2b395c6
gaoooon <gaon9749@gmail.com>
2024-02-23 18:15:13 +0900

    Add style.ts

---
+++ packages/ui/src/components/BoxContainer/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CounterContainer = styled.div`
+  width: 18.75rem;
+  height: 25rem;
+  border-radius: 1.875rem;
+  background: #d9d9d9;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
