7ef6a64965fb9477b350b706b4caa1b817bbdf47
gaoooon <gaon9749@gmail.com>
2024-02-24 00:27:26 +0900

    update main.tsx

--- apps/zustand/src/main.tsx
+++ apps/zustand/src/main.tsx
@@ -1,4 +1,10 @@
 import ReactDOM from "react-dom/client";
+import GlobalStyle from "./globalStyle";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
